b0a87074422a7e344e482e5cf4fb2de663391bbd
saumya <saumyawardhan1@gmail.com>
2018-06-30 23:14:23 +0530

    finish auth flow

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const keys = require('./config/keys.js');
+
+require('./models/User');
+require('./services/passport.js');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({hi: 'saumya'});
-});
+app.use(
+	cookieSession({
+		maxAge: 30*24*60*60*1000,
+		keys: [keys.cookieKey]
+	})
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
